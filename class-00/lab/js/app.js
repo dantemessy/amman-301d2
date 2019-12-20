1d00655a275e3e9ad3f82644fa65be237858436e
dantemessy <thepunishar1@gmail.com>
2019-12-20 18:05:01 +0200

    Done

--- class-00/lab/js/app.js
+++ class-00/lab/js/app.js
@@ -30,12 +30,12 @@ function makeRandom() {
 
 function displayPics(){
   while(viewed.length < 6){
-    var rando = makeRandom();
+    let rando = makeRandom();
     while(!viewed.includes(rando)){
       viewed.push(rando);
     }
   }
-  console.log(rando);
+  // console.log(rando);
   // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the constiable declaration from `const to `const`.
   // PUT YOUR RESPONSE IN THIS COMMENT
   // because we used const inside the function so the declaration can be scoped to a code block only ;
@@ -63,7 +63,7 @@ function handleClick(event) {
   for(let i = 0; i < names.length; i++){
     if(event.target.id === allProducts[i].name) {
       allProducts[i].votes += 1;
-      console.log(event.target.id + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views');
+      console.log(`event.target.id +  has  + ${allProducts[i].votes} + votes in  + ${allProducts[i].views} +  views`);
     }
   }
   localStorage.busmall = JSON.stringify(allProducts);
@@ -74,7 +74,7 @@ function handleClick(event) {
 function showList() {
   for(let i = 0; i < allProducts.length; i++) {
     const liEl = document.createElement('li');
-    liEl.textContent = allProducts[i].name + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views';
+    liEl.textContent = `${allProducts[i].name} +  has  + ${allProducts[i].votes} +  votes in  + ${allProducts[i].views} +  views`;
     list.appendChild(liEl);
   }
 }
@@ -129,7 +129,7 @@ if(localStorage.busmall){
   allProducts = JSON.parse(localStorage.busmall);
 } else {
   console.log('There is no local storage data; initialize app by creating instances');
-  for(var i = 0; i < names.length; i++) {
+  for(let i = 0; i < names.length; i++) {
     new Product(names[i]);
   }
 }
